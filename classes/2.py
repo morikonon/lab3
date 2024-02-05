4018a72ce0c6ac22aeaf36dd75598e8196a35dc5
Mukhamedali Daniyaruly <153483615+morikonon@users.noreply.github.com>
2024-02-05 19:00:01 +0600

    Update 2.py

--- classes/2.py
+++ classes/2.py
@@ -1,3 +1,15 @@
 class Shape:
-	class Square:
-		def __init__(self , length)
+	def __init__(self):
+		pass
+	def area(self):
+		return 0
+class Square(Shape):
+	def __init__(self,lenght):
+		super().__init__(self)
+		self.lenght = lenght
+	def area(self):
+		return self.lenght ** 2
+shape = Shape()
+print("Area of shape" , shape.area())
+square = Square(5)
+print("Area of Square" , square.area())
